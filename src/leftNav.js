e8a07d6cd789a3e2d6cfa9f6584233cdef392607
Rich Devine <richard.devine@gmail.com>
2019-02-07 11:54:50 -0800

    Added Routing to NavLinks

--- src/leftNav.js
+++ src/leftNav.js
@@ -3,80 +3,87 @@ import AppRouter from './AppRouter1';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const navList = {
-	userLinks : {
-		releaseInformation : {
-			linkText : 'Release Information'
-		}, 
-
-		projectContacts : {
-			linkText : 'Project Contacts'
-		}, 
-
-		audioFiles : {
-			linkText : 'Audio Files (#)' 
-		}, 
-
-		trackInfo :{
-			linkText : 'Track Information (#)'
-		},
-
-		territorialRights: {
-			linkText : 'Territorial Rights'
-		},
-
-		blockingPolicies : {
-			linkText : 'Blocking Policies'
-		},
-
-		reviewSubmit : {
-			linkText : 'Review & Submit'
-		},
+	releaseInformation : {
+		linkText : 'Release Information',
+		navRoute : '/releaseInformation',
+		userAccess : 'all'
 	}, 
 
-	adminLinks : {
-		newProject : {
-			linkText : 'New Project'
-		},
+	projectContacts : {
+		linkText : 'Project Contacts',
+		navRoute : '/projectContacts',
+		userAccess : 'all'
+	}, 
 
-		findProject : {
-			linkText : 'Find a Project'
-		},
+	audioFiles : {
+		linkText : 'Audio Files (#)',
+		navRoute : '/audioFiles',
+		userAccess : 'all'
+	}, 
 
-		recentProjects : {
-			linkText : 'Find a Project'
-		}
+	trackInformation :{
+		linkText : 'Track Information (#)',
+		navRoute : '/trackInformation',
+		userAccess : 'all'
+	},
+
+	territorialRights: {
+		linkText : 'Territorial Rights',
+		navRoute : '/territorialRights',
+		userAccess : 'all'
+	},
+
+	blockingPolicies : {
+		linkText : 'Blocking Policies',
+		navRoute : '/blockingPolicies',
+		userAccess : 'all'
+	},
+
+	reviewSubmit : {
+		linkText : 'Review & Submit',
+		navRoute : '/reviewSubmit',
+		userAccess : 'all'
+	},
+
+	newProject : {
+		linkText : 'New Project',
+		navRoute : '/newProject',
+		userAccess : 'admin'
+	},
+
+	findProject : {
+		linkText : 'Find a Project',
+		navRoute : '/findProject',
+		userAccess : 'admin'
+	},
+
+	recentProjects : {
+		linkText : 'Find a Project',
+		navRoute : '/findProject',
+		userAccess : 'admin'
 	}
 };
-
 
 const MakeUserNavLinks = (props) => {
 	//console.log('MakeUserNavLinks : ' + props.linkText)
 	return(
-		<li key={props.index}><a className="steps" href="release-information.html">Step <span className="count-circle"></span> {props.linkText}</a> <span className="checkbox"></span></li>
-	)
-};
+		<li key={props.index}>
+			<Link to={props.navRoute}>
+				Step <span className="count-circle"></span> {props.linkText}
+			</Link>
+			<span className="checkbox"></span>
 
-const MakeAdminNavLinks = (props) => {
-	//console.log('MakeAdminNavLinks : '  + props.linkText)
-	return(
-		<li key={props.index}><a href="release-information.html">{props.linkText}</a></li>
+			<Route path="{props.navRoute}" exact component={props.linkText} />
+		</li>
 	)
 };
 
 const LeftNav = () => {
 
-	const userLinks = Object.keys(navList.userLinks).map(
+	const userLinks = Object.keys(navList).map(
 		function(link, i) { 
 			return(
-				<MakeUserNavLinks key={i} index={i} linkText={navList.userLinks[link].linkText} />
-			)
-		}
-	);
-
-	const adminLinks = Object.keys(navList.userLinks).map(
-		function(link, i) { 
-			return(
-				<MakeAdminNavLinks key={i} index={i} linkText={navList.userLinks[link].linkText} />
+				<MakeUserNavLinks key={i} index={i} linkText={navList[link].linkText} navRoute={navList[link].navRoute}/>
 			)
 		}
 	);
@@ -85,12 +92,13 @@ const LeftNav = () => {
 		<nav className="left-nav col-2">
 			<section className="fixed-left-nav col-2">
 				<span className="left-nav-logo"></span>
-				<ul>
-					{userLinks}
-					{adminLinks}
-				</ul>
-			</section>
+				<Router>
+					<ul>
+						{userLinks}
+					</ul>
+				</Router>
 
+			</section>
             <AppRouter />
 		</nav>
     )
