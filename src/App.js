79e1aef33b6b872458d66b7663591dcfddc874b1
Rich Devine <richard.devine@gmail.com>
2019-02-08 22:12:38 -0800

    Initial Layout Changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import LeftNav from './leftNav';
+
+import LeftNav from './components/leftNav';
+import Content from './content';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-
+      <div style={{border: 'solid 1 red', position: 'relative'}}>
           <LeftNav />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <Content />
       </div>
+
+
     );
   }
 }
