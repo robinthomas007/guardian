8b474b2b0d58a842ab88421894b54cd863f00d06
Robert Lestak <robert.lestak@umusic.com>
2019-02-26 00:29:33 -0800

    Enironment-agnostic build

--- src/App.js
+++ src/App.js
@@ -2,15 +2,10 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route} from "react-router-dom";
 import {LoginPage} from './components/pages';
 import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
-
-const config = {
-  issuer: 'https://dev-192402.oktapreview.com/oauth2/default',
-  redirect_uri: window.location.origin + '/implicit/callback',
-  client_id: '0oaji620egWzO8C8A0h7'
-}
+import config from './config';
 
 class App extends Component {
-  
+
   constructor(props) {
     super(props);
 
@@ -23,9 +18,9 @@ class App extends Component {
   render() {
     return(
      <BrowserRouter>
-        <Security issuer={config.issuer}
-          client_id={config.client_id}
-          redirect_uri={config.redirect_uri}
+        <Security issuer={config.okta.issuer}
+          client_id={config.okta.client_id}
+          redirect_uri={config.okta.redirect_uri}
         >
           <Route exact path="/" component={LoginPage} />
           <Route path='/implicit/callback' component={ImplicitCallback} />
