1920ecbf620715d8011ae740091517b0c65b46a1
Rich Devine <richard.devine@gmail.com>
2019-05-06 10:22:09 -0700

    GRD-99
    Added cookie setter / getter to show modal

--- src/components/modals/IntroModal.js
+++ src/components/modals/IntroModal.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {Button, Modal } from 'react-bootstrap';
 import './IntroModal.css';
+import Cookies from 'universal-cookie';
+
 
 class IntroModal extends Component {
   constructor(props, context) {
@@ -11,14 +13,23 @@ class IntroModal extends Component {
 
     this.state = {
       show: false,
+      doNotShow : false
     };
+
+    this.doNotShowAgain = this.doNotShowAgain.bind(this);
   }
 
-  doNotShowAgain () {
-
+  doNotShowAgain(event) {
+    this.setState({ doNotShow: true });
   }
 
-  handleClose() {
+  handleClose(event) {
+    if(this.state.doNotShow) {
+      const cookies = new Cookies();
+      const todaysDate = new Date();
+      const futureDate = new Date(todaysDate.getFullYear() + 100, todaysDate.getMonth(), todaysDate.getDate())
+      cookies.set('guardianShowLoginModal', false, { path:'/', expires: futureDate });
+    }
     this.setState({ show: false });
   }
 
@@ -30,59 +41,79 @@ class IntroModal extends Component {
     this.handleShow()
   }
 
-  render() {
-    return (
-      <>
-        <Modal id='IntroModal' size="lg" show={this.state.show} onHide={this.handleClose}>
-        <div className='red-diag'>
-        <div className='purp-diag'>
-          <Modal.Header  closeButton>
-            <Modal.Title>Welcome to the Guardian!</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            UMG's Content Protection Guardian application is a 'one-stop-shop' for all of your content protection and anti-piracy needs. Here, users can set up new and upcoming releases for protection services, including:
-            <ul>
-                <li>
-                    <div className='leak-detection'></div>
-                    <div>Content Protection, Leak Detection &amp; Monitoring</div>
-                </li>
-                <li>
-                    <div className='web-crawling'></div>
-                    <div>Web-Crawling &amp; Take-downs from Unlicensed Platforms</div>
-                </li>
-                <li>
-                    <div className='territorial-rights'></div>
-                    <div>Establish Territorial Rights &amp; Set Post-Release UGC Blocking Policies</div>
-                </li>
-            </ul>
+  getModal() {
+    const cookies = new Cookies();
+    if(!cookies.get('guardianShowLoginModal')) {
+      return(
+        <>
+          <Modal id='IntroModal' size="lg" show={this.state.show} onHide={this.handleClose}>
+            <div className='red-diag'>
+              <div className='purp-diag'>
+                <Modal.Header  closeButton>
+                  <Modal.Title>Welcome to the Guardian!</Modal.Title>
+                </Modal.Header>
+          
+                <Modal.Body>
+                  UMG's Content Protection Guardian application is a 'one-stop-shop' for all of your content protection and anti-piracy needs. Here, users can set up new and upcoming releases for protection services, including:
+                  <ul>
+                      <li>
+                          <div className='leak-detection'></div>
+                          <div>Content Protection, Leak Detection &amp; Monitoring</div>
+                      </li>
+                      <li>
+                          <div className='web-crawling'></div>
+                          <div>Web-Crawling &amp; Take-downs from Unlicensed Platforms</div>
+                      </li>
+                      <li>
+                          <div className='territorial-rights'></div>
+                          <div>Establish Territorial Rights &amp; Set Post-Release UGC Blocking Policies</div>
+                      </li>
+                  </ul>
+                  Feel free to click on any of the helpful informational buttons scattered throughout the site but please also contact us at the address below. We are here to help you achieve success for your artists!
+                  
+                  <br />
+                  <br />
 
-            Feel free to click on any of the helpful informational buttons scattered throughout the site but please also contact us at the address below. We are here to help you achieve success for your artists!
-            <br />
-            <br />
-            <a href="#">Have a question or need help getting started? Click Here</a>
-            <br />
-            <br />
-           </Modal.Body>
-          <Modal.Footer>
-              <div className="show-check">
-              <label className="custom-checkbox"> 		
-                                    <input id='doNotShowIntro'  
-                                        type='checkbox' 
-                                        onClick={this.handleClose}
-                                    />
-                                    <span className="checkmark "></span>
-                                </label>
-                                <label class="noshow-label">Do not show this anymore.</label>
+                  <a href="#">Have a question or need help getting started? Click Here</a>
+
+                  <br />
+                  <br />
+                </Modal.Body>
+          
+                <Modal.Footer>
+                  <div className="show-check">
+                    <label className="custom-checkbox"> 		
+                      <input
+                        id='doNotShowIntro'  
+                        type='checkbox' 
+                        onChange={this.doNotShowAgain}
+                      />
+                      <span className="checkmark "></span>
+                    </label>
+                    <label class="noshow-label">Do not show this anymore.</label>
+                  </div>
+            
+                  <Button variant="primary" onClick={this.handleClose}>
+                    Continue
+                  </Button>
+                </Modal.Footer>
               </div>
-            <Button variant="primary" onClick={this.handleClose}>
-              Continue
-            </Button>
-          </Modal.Footer>
-          </div>
-          </div>
-        </Modal>
+            </div>
+          </Modal>
+        </>
+      )
+    } else {
+      return(null)
+    }
+  }
+
+  render() {
+    return(
+      <>
+        {this.getModal()}
       </>
-    );
+      
+    )    
   }
 }
 
