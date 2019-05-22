fbb34fac09558bdb5aba582917ee2d0476329c89
Ryan Christofferson <ryan.christofferson@gmail.com>
2019-05-22 11:28:58 -0700

    GRD-115
    Added Request Access Modal

--- src/components/pages/HomePage/HomePage.js
+++ src/components/pages/HomePage/HomePage.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
+import RequestAccessModal from '../../modals/RequestAccessModal';
 import './HomePage.css'
 
 class HomePage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showRequestModal : false
+        }
+        this.showRequestModal = this.showRequestModal.bind(this);
+        this.hideRequestModal = this.hideRequestModal.bind(this);
+    }
 
     state = {
         redirect : false
@@ -14,11 +24,20 @@ class HomePage extends Component {
         
     }
 
+    showRequestModal() {
+        this.setState({showRequestModal : true})
+    }
+
+    hideRequestModal() {
+        this.setState({showRequestModal : false})
+    }
+
     render() {
 
         return(
 
             <section className="container-fluid landing">
+                <RequestAccessModal showModal={this.state.showRequestModal} handleClose={this.hideRequestModal}/>
                 <section className="logo"><img src="/static/images/guardian-logo.png" /></section>
                 <nav className="top-nav ext">
                     <ul>
@@ -32,7 +51,7 @@ class HomePage extends Component {
                     <h1>WELCOME TO THE GUARDIAN</h1>
                     <h2>CONTENT PROTECTION, LEAK DETECTION &amp; ANTI-PIRACY</h2>
                     <span>
-                        <button id="loginRequestAccess" className="access btn">Request Access</button>
+                        <button id="loginRequestAccess" className="access btn" onClick={this.showRequestModal}>Request Access</button>
                         <button id="loginLogIn" className="log-in btn" onClick={this.login}>Log In</button>
                     </span>
                 </section>
