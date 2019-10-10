04bf25f92bd100ecf85b73c7046046d34897a606
Ryan Christofferson <ryan.christofferson@gmail.com>
2019-10-10 13:15:46 -0700

    Adding CSS

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import '../src/css/index.css'
 import '../src/css/noty.css'
 import '../src/css/bootstrap-v4.css'
+import '../src/css/header.css'
 import LoginPage from './components/pages/HomePage/HomePage';
 import Content from "./components/content";
 import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
