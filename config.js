b64733eef650679cc8334f10e0a387f5c27eac3d
Robert Lestak <robert.lestak@umusic.com>
2019-10-22 16:23:07 -0700

    Default login method to ldap

--- config.js
+++ config.js
@@ -56,13 +56,15 @@ async function vaultGetSecret(token, secret) {
         break;
       case "userpass":
         token = await vaultUserPassLogin(process.env.VAULT_USER, process.env.VAULT_PASS)
+	break;
       default:
-        token = await vaultUserPassLogin(process.env.VAULT_USER, process.env.VAULT_PASS)
+        token = await vaultLdapLogin(process.env.VAULT_USER, process.env.VAULT_PASS)
+	break;
     }
     process.env.NODE_ENV = process.env.NODE_ENV.toLowerCase()
     let data = await vaultGetSecret(token, 'guardian-ui/' + process.env.NODE_ENV)
     fs.writeFileSync("src/config/index.json", JSON.stringify(data))
   } catch(e) {
-    console.log(e)
+    console.log(e.message)
   }
 })();
