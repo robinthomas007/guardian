8f272efa097aa3082a8facd6607815d19ed14d0b
Bo Li <bosupreme@gmail.com>
2019-10-23 13:18:31 -0400

    Fix putting field in valid state, and valid bug

--- src/components/Utils.js
+++ src/components/Utils.js
@@ -1,76 +1,73 @@
 export const isFormValid = () => {
     let requiredInputs = document.getElementsByClassName('requiredInput');
-    let invalid = false;
 
-    for(var i=0; i<requiredInputs.length; i++) {
-        let input = requiredInputs[i];
-        if(input.value.length <= 0) {
-            setInputValidStatus(input, 'invalid');
-            invalid = false;
-        } else if (input.type === 'email') {
-            if(isValidEmail(input.value)) {
-                setInputValidStatus(input, 'valid');
-                invalid = false;
-            } else {
+    return Array.prototype.map
+        .call(requiredInputs, input => {
+            if (input.value.length <= 0) {
                 setInputValidStatus(input, 'invalid');
-                invalid = true;
+                return false;
+            } else if (input.type === 'email') {
+                if (isValidEmail(input.value)) {
+                    setInputValidStatus(input, 'valid');
+                    return true;
+                } else {
+                    setInputValidStatus(input, 'invalid');
+                    return false;
+                }
+            } else {
+                setInputValidStatus(input, 'valid');
+                return true;
             }
-        } else {
-            setInputValidStatus(input, 'valid');
-            invalid = true;
-        }
-    }
-
-    return(invalid)
-}
+        })
+        .every(isValid => isValid === true); // Check if all fields are valid
+};
 
 export const setInputValidStatus = (input, status) => {
-    if(status === 'invalid') {
+    if (status === 'invalid') {
         input.className = input.className.replace('is-invalid', '') + ' is-invalid';
     } else {
-        input.className.replace('is-invalid', '');
+        input.className = input.className.replace('is-invalid', '');
     }
-}
+};
 
-export const isValidIsrc = (isrc) => {
-    return((isrc.replace(/\W/g, '').length == 12) ? true : false);
-}
+export const isValidIsrc = isrc => {
+    return isrc.replace(/\W/g, '').length == 12 ? true : false;
+};
 
-export const isValidTitle = (title) => {
-    return((title.length > 0) ? true : false);
-}
+export const isValidTitle = title => {
+    return title.length > 0 ? true : false;
+};
 
-export const isValidEmail = (email) => {
-    return ( /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,64})+$/.test(email) )
-}
+export const isValidEmail = email => {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,64})+$/.test(email);
+};
 
-export const convertToLocaleTime = (dateString) => {
-
+export const convertToLocaleTime = dateString => {
     const utcDate = new Date(dateString);
-          utcDate.setSeconds(0,0);
+    utcDate.setSeconds(0, 0);
     const localTime = utcDate.toLocaleString();
-        
-    let dateArr = localTime.split(' ')
-    let date = dateArr[0].replace(',', '')
-    let timeArr =  dateArr[1].split(':')
-    let amPm = (dateArr[2]) ? dateArr[2] : ''
-    let dateStr = date + ' ' + timeArr[0] + ':' + timeArr[1] + ' ' + amPm
 
-    return (dateStr)
-}
+    let dateArr = localTime.split(' ');
+    let date = dateArr[0].replace(',', '');
+    let timeArr = dateArr[1].split(':');
+    let amPm = dateArr[2] ? dateArr[2] : '';
+    let dateStr = date + ' ' + timeArr[0] + ':' + timeArr[1] + ' ' + amPm;
 
-export const formatDateToYYYYMMDD = (unFormattedDate) => {
+    return dateStr;
+};
+
+export const formatDateToYYYYMMDD = unFormattedDate => {
     let formattedDate = '';
-    if(unFormattedDate) {
+    if (unFormattedDate) {
         var d = new Date(unFormattedDate),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear();
 
         if (month.length < 2) month = '0' + month;
         if (day.length < 2) day = '0' + day;
 
         formattedDate = [year, month, day].join('-');
     }
-    return(formattedDate)
-}
+    return formattedDate;
+};
