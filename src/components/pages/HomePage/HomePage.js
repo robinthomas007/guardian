0707090374d8c382e3837ae4b9c9ecd4f3c97e9d
Venkateshwaran <Venkatgillly>
2020-02-11 22:18:08 +0530

    Okta widget integration

--- src/components/pages/HomePage/HomePage.js
+++ src/components/pages/HomePage/HomePage.js
@@ -2,33 +2,71 @@ import React, { Component } from 'react';
 import RequestAccessModal from '../../modals/RequestAccessModal';
 //import OktaSignInWidget from 'OktaSignInWidget';
 import Noty from 'noty';
-import './HomePage.css'
+import './HomePage.css';
 
 
-class HomePage extends Component {
+import {withAuth} from '@okta/okta-react';
+
+
+
+export default withAuth(class HomePage extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
             showRequestModal : false,
-            showOktaWidget : false
+            showOktaWidget : false,
+            authenticated: null
         }
         this.showRequestModal = this.showRequestModal.bind(this);
         this.hideRequestModal = this.hideRequestModal.bind(this);
+        
         // this.showOktaWidget = this.showOktaWidget.bind(this);
         // this.hideOktaWidget = this.hideOktaWidget.bind(this);
+
+        //this.state = { authenticated: null };
+        this.checkAuthentication = this.checkAuthentication.bind(this);
+        this.login = this.login.bind(this);
+        this.logout = this.logout.bind(this);
     }
+
+
+
+     async checkAuthentication() {
+        const authenticated = await this.props.auth.isAuthenticated();
+        if (authenticated !== this.state.authenticated) {
+          this.setState({ authenticated });
+        }
+      }
+    
+      async componentDidMount() {
+        this.checkAuthentication();
+      }
+    
+      async componentDidUpdate() {
+        this.checkAuthentication();
+      }
+    
+      async login() {
+        this.props.auth.login('/');
+      }
+    
+      async logout() {
+        this.props.auth.logout('/');
+      }
+
+
 
     state = {
         redirect : false
     }
 
-    login = () => {
-        return(
-            this.props.history.push('/findProject')
-        )
+    // login = () => {
+    //     return(
+    //         this.props.history.push('/findProject')
+    //     )
         
-    }
+    // }
 
     showRequestModal() {
         this.setState({showRequestModal : true})
@@ -76,6 +114,8 @@ class HomePage extends Component {
 
     render() {
 
+        if (this.state.authenticated === null) return null;
+
         return(
 
             <section className="container-fluid landing">
@@ -95,6 +135,10 @@ class HomePage extends Component {
                     <h2>CONTENT PROTECTION, LEAK DETECTION &amp; ANTI-PIRACY</h2>
                     <span>
                         <button id="loginRequestAccess" className="access btn" onClick={this.showRequestModal}>Request Access</button>
+                        {/* <button id="loginLogIn" className="log-in btn" onClick={this.login}  */}
+                            {/* /* onClick={this.props.showOktaWidget} */}
+                            {/* // > */}
+                            {/* Log In</button> */}
                         <button id="loginLogIn" className="log-in btn" onClick={this.login} /* onClick={this.props.showOktaWidget} */ >Log In</button>
                     </span>
                 </section>
@@ -104,6 +148,6 @@ class HomePage extends Component {
             </section>
         )
     }
-};
+});
 
-export default HomePage;
+//export default HomePage;
