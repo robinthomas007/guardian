39a9801675ef3ba66a66f7716a4970998c63faff
Vinoth Periyasamy <vinothperiyasamy@AGLS-MLT-579.local>
2023-04-11 10:50:51 +0530

    add flags for languageDropdown

---
+++ src/images/index.js
@@ -0,0 +1,11 @@
+import French from './flags/french.png';
+import German from './flags/german.png';
+import English from './flags/US.png';
+import Spanish from './flags/spanish.png';
+const images = {
+  French,
+  German,
+  English,
+  Spanish,
+};
+export default images;
