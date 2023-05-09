445f68e45a35e517f3d815a11934b56608e06909
Vinoth Periyasamy <vinothperiyasamy@AGLS-MLT-579.local>
2023-05-09 15:32:03 +0530

    releaseInfo Import bugfix

--- src/components/pages/ReleaseInformation/ReleaseInformationPage.js
+++ src/components/pages/ReleaseInformation/ReleaseInformationPage.js
@@ -7,7 +7,7 @@ import LoadingImg from 'component_library/LoadingImg';
 import { resetDatePicker, isFormValid, CustomInput, NO_LABEL_ID } from '../../Utils.js';
 import moment from 'moment';
 import ReleasingLabelsInput from '../ReleaseInformation/pageComponents/ReleasingLabelsInput';
-import MultiSelectHierarchy from '../../common/MultiSelectHierarchy.js';
+import MultiSelectHierarchy from '../../common/MultiSelectHierarchy';
 import ProjectTypesInput from '../ReleaseInformation/pageComponents/ProjectTypesInput';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
