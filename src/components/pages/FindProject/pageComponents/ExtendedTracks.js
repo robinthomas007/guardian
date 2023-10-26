27e73c805669d32902ccaf665ab0f6362b61ed39
Vinoth Periyasamy <vinothperiyasamy@AGLS-MLT-579.local>
2023-10-26 12:40:01 +0530

    changed extended tracks

--- src/components/pages/FindProject/pageComponents/ExtendedTracks.js
+++ src/components/pages/FindProject/pageComponents/ExtendedTracks.js
@@ -118,8 +118,8 @@ const Tracks = ({
           <th>{t('track:ReleaseDate')}</th>
           <th>{t('track:Deliver')}</th>
           <th className="status text-center">{t('track:HasRights')}</th>
+          <th>{'Asset'}</th>
           <th>{t('track:Deliver')}?</th>
-          <th>under 30 seconds?</th>
           {/*under 30 sec? to convert  all languages and place it in global file   */}
         </tr>
       </thead>
@@ -174,19 +174,19 @@ const Tracks = ({
                     <i className="material-icons success">verified_user</i>
                   )}
                 </td>
-                <td>
-                  <Toggle
-                    defaultChecked={Boolean(track.isPublish)}
-                    onChange={() => onIsPublishToggle(project, track.trackID)}
-                    disabled={project.status === 'In Progress'}
-                  />
-                </td>
                 <td className="status">
                   {track.isLessThan30Secs ? (
                     <i className="material-icons warning">report_problem</i>
                   ) : (
                     <i className="material-icons success">verified_user</i>
                   )}
+                </td>
+                <td>
+                  <Toggle
+                    defaultChecked={Boolean(track.isPublish)}
+                    onChange={() => onIsPublishToggle(project, track.trackID)}
+                    disabled={project.status === 'In Progress'}
+                  />
                 </td>
               </tr>
               {extendedTrackIds.includes(track.trackID) ? (
